ba2ccaab325ec3683eeb2ba45f00ae0ecef7d9e1
十下 <49399610+lincheng77@users.noreply.github.com>
2023-08-23 09:34:31 +0800

    Update server.ts

--- app/config/server.ts
+++ app/config/server.ts
@@ -37,8 +37,13 @@ export const getServerSideConfig = () => {
     );
   }
 
+  //start
+  const apiKeys = (process.env.OPENAI_API_KEY ?? '').split(',')
+  const apiKey = apiKeys.at(Math.floor(Math.random() * apiKeys.length)) ?? ''
+
   return {
-    apiKey: process.env.OPENAI_API_KEY,
+    // apiKey: process.env.OPENAI_API_KEY,
+    apiKey,
     code: process.env.CODE,
     codes: ACCESS_CODES,
     needCode: ACCESS_CODES.size > 0,
